1cbbe42924118fadd5241f10facddb099f7af505
Andrew Culhane <andrewculhane@Andrews-MBP.fios-router.home>
2020-08-14 22:37:52 -0400

    test

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,9 +1,9 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do 
-    # allow do 
-    #     origins "http://localhost:3000"
-    #     resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
-    # credentials: true 
-    # end 
+    allow do 
+        origins "http://localhost:3000"
+        resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
+    credentials: true 
+    end 
     allow do 
         # Production app is going to be on this.
         origins "https://goofy-goldstine-c936b6.netlify.app"
