bb33fa11baea76a8c2e38ef642f1aa6b6d7c7438
Andrew Culhane <andrewculhane@Andrews-MBP.fios-router.home>
2020-08-15 13:31:04 -0400

    test

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == "production"
-Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "https://test-dreamway-1234.herokuapp.com/"
+Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "test-dreamway-1234.herokuapp.com/"
 else 
     Rails.application.config.session_store :cookie_store, key: "_authentication_app"
 end 
